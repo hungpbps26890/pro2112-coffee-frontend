8984625ee0da41af37e84b7aeb43c03630616120
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2024-06-06 10:17:53 +0700

    Add Input FormControl, Form Login and Register

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Menu from "./pages/Menu/Menu";
 import DrinkDetail from "./pages/DrinkDetail/DrinkDetail";
 import Cart from "./pages/Cart/Cart";
 import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
         <Route path="/menu/category/:id" element={<Menu />} />
         <Route path="/drinks/:id" element={<DrinkDetail />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </>
